c833ab5514bfb7218f3fb690c30b150801af1516
anantadwi13@yahoo.com <tcits17@tukangkode.com>
2018-12-19 08:41:16 +0700

    update api route

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::post('/getdistrict', 'API@getDistrict')->name('api.getDistrict');
 Route::get('/getreservasi', 'API@getReservasi')->name('api.getReservasi');
